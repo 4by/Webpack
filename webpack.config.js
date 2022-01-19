be21443b61ea80adc7b74cd47a4ac283f29bbdc6
idoodi <idoodi@ya.ru>
2022-01-19 03:58:43 +0300

    базовая_настройка_webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
 const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+
+//process это обьект Node.js
+const isDev = process.argv[3] === 'development'
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+
+
+
+
+
 
 module.exports = {
-    entry: './src/index.js',
+    context: path.resolve(__dirname, 'src'),
+    //файлы, импортирующиеся в сборочный index.html
+    entry: {
+        main: './index.js',
+        analytics: "./analytics.js"
+    },
+    //как преобразуется входящий файл
     output: {
-        filename: 'bundle.js',
+        filename: filename('js'),
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+
+
+
+    plugins: [
+        //создает в сборке файл index.html
+        new htmlWebpackPlugin({
+
+            //отправляет информацию в тег <title>
+            // title: "Webpack Eugene",
+
+            //документ на основе которого происходит сборка
+            template: './index.html',
+        }),
+    ],
 }
